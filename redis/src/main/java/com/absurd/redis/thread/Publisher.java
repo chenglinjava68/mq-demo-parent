86c4a04a34afa01d73ca6c294b0934977dc40864
www1350 <www_1350@163.com>
2016-12-15 20:44:52 +0800

    @add p/b

--- redis/src/main/java/com/absurd/redis/thread/Publisher.java
+++ redis/src/main/java/com/absurd/redis/thread/Publisher.java
@@ -5,7 +5,9 @@ import com.absurd.redis.springdata.listen.RedisMessageListener;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -21,7 +23,8 @@ import java.util.concurrent.atomic.AtomicInteger;
  * @Description:
  * @date 2016/12/15 20:23
  */
-@Component
+@Configuration
+@EnableScheduling
 public class Publisher {
 
     private static Logger logger = LoggerFactory.getLogger(Publisher.class);
@@ -29,7 +32,7 @@ public class Publisher {
     @Autowired
     private StringRedisTemplate stringRedisTemplate;
     private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
-    @Scheduled(fixedRate = 5000)
+    @Scheduled(cron = "0/20 * * * * ?")
     public void reportCurrentTime() {
         String channel = "user:topic1";
         stringRedisTemplate.convertAndSend(channel, "发布一个项目"+atomicInteger.incrementAndGet()+"The time is now " + dateFormat.format(new Date()));
